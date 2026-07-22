9fb34c04de41f327dad9a8fb962f7f9dd4f2e83a
johnathan-clause <34804777+johnathan-clause@users.noreply.github.com>
2026-07-22 18:49:33 -0500

    Create crystal.lua

---
+++ settings/crystal.lua
@@ -0,0 +1,23 @@
+xi = xi or {}
+xi.settings = xi.settings or {}
+
+xi.settings.crystal = 
+{
+    -- %Max HP/MP to heal per tick from resting
+    RESTING_HEALTH_PERCENT_RESTORE                              =   2,
+    RESTING_MANA_PERCENT_RESTORE                                =   2,
+
+    -- duration in seconds
+    MEIKYO_SHISUI_EFFECT_DURATION                               =   15,
+
+    -- duration in seconds
+    CHOCOBO_WHISTLE_EFFECT_DURATION                             =   43200,
+    CHOCOBO_WHISTLE_MIN_LEVEL                                   =   1,
+    CHOCOBO_WHISTLE_NEED_LICENSE                                =   false,
+
+    -- duration in seconds
+    LIZARD_BALEFUL_GAZE_EFFECT_DURATION                         =   30,
+
+    -- duration in seconds
+    COCKATRICE_BALEFUL_GAZE_EFFECT_DURATION                     =   30,
+}
